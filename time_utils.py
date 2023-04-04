ad0f42759fc7002e0ab884dbd3c5ea24c0c5cc0a
mafits <costamafalda7@gmail.com>
2023-04-04 22:44:14 +0100

    comment time utlils

--- time_utils.py
+++ time_utils.py
@@ -1,20 +1,13 @@
-import networkx as nx
-from datetime import datetime, timedelta
-
-
+# checks if an establishment is open at a specific hour
 def is_open(establishment, hour):
     availableHours = eval(establishment['openingHours'])
-    # print('hour: ', hour)
-    # print('available hours: ', availableHours)
     if availableHours[hour % 24]:
         return True
     else:
         return False
 
-
+# returns the next hour at which the establishment is open, given the arrival hour
 def next_open_hour(establishment, hour):
-    # Returns the next hour at which establishment is open given the arrival hour.
-    # Find the next occurrence of 1 in the list after the given hour
     availableHours = eval(establishment['openingHours'])
     index = (hour % 24)
     while index < 24 and availableHours[index] == 0:
@@ -27,7 +20,7 @@ def next_open_hour(establishment, hour):
     return index
 
 
-# format_time
+# takes seconds and transforms them into the string hour.min.sec
 def seconds_to_string(seconds):
     hours = (int)(seconds // 3600)
     minutes = (seconds % 3600) // 60
@@ -38,7 +31,7 @@ def seconds_to_string(seconds):
     return time_str
 
 
-# time_to_seconds
+# takes the string hour.min.sec and transforms it into seconds
 def string_to_seconds(time_str):
     hours, minutes, seconds = time_str.split('.')
 
@@ -51,42 +44,37 @@ def string_to_seconds(time_str):
     return total_seconds
 
 
-# time_to_hour
+# takes the string and returns the hour
 def string_hours(time_str):
-    # print(time_str)
     hours, _, _ = time_str.split('.')
     hours = int(hours)
-    # print(hours%24)
     return hours
 
-
+# calculates the time of a solution - the bigger arrival time amongst the vans
 def total_time(solution):
     flat_list = [path[-1] for path in solution]
-    # Find the maximum time value using max() function and a lambda function
     max_time = max(flat_list, key=lambda x: string_to_seconds(x[1]))
-
     return max_time[1]
 
 
-def waiting_time(graph, currentTime, establishment):
+# knowing the time of arrival, calculates how long a van has to wait for the establishment to open
+def waiting_time(currentTime, establishment):
     hourAtArrival = (string_hours(seconds_to_string(currentTime))) % 24  # hour at arrival on current node rounded down
-    # print(hourAtArrival)
     if (is_open(establishment, hourAtArrival)):
         waitingTime = 0
     else:
         next_open_seconds = next_open_hour(establishment, hourAtArrival) * 3600
-        # ?waiting time could cross to the next day, so we need to check if the next open hour is after the current hour
+        # waiting time could cross to the next day, so we need to check if the next open hour is after the current hour
         if (next_open_seconds > currentTime):
             waitingTime = (next_open_seconds - currentTime)
         else:
             waitingTime = ((24 * 3600) - (currentTime % (24 * 3600)) + next_open_seconds)
 
-    # print('waiting time: ', waitingTime % 86400)
     return (waitingTime % 86400)
 
 
+# calculates the times for each node in the path based on the updated times of the previous nodes and the travel times between them
 def recalculate_hours(graph, changedPath):
-    # print('\nsem alteracao: ',changedPath)
     for i in range(1, len(changedPath)):
         time = string_to_seconds(graph.edges[changedPath[i - 1][0], changedPath[i][0]]['travelTime']) + string_to_seconds(changedPath[i - 1][1])
         waitingTime = waiting_time(graph, string_to_seconds(changedPath[i - 1][1]), graph.nodes[changedPath[i - 1][0]])
@@ -95,11 +83,11 @@ def recalculate_hours(graph, changedPath):
             time += waitingTime + (graph.nodes[changedPath[i - 1][0]]['inspectionDuration'])
 
         changedPath[i] = (changedPath[i][0], seconds_to_string(time))
-    # print('alterado: ',changedPath)
 
     return changedPath
 
 
+# calculates the arrival time at an establishment, knowing the previous establishment infos and travel time between both establishments
 def arrival_time(prev_arrival_time, prev_establishment, inspection_time, travel_time):
     prev_arrival_hour = string_hours(prev_arrival_time)
     # waiting time
